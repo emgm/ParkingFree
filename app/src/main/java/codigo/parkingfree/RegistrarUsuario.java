a54972e483a8677e0f053e05e593d71bd4ee858e
DESARROLLO-07 <emgonzalezm@ufpso.edu.co>
2018-11-21 15:33:22 -0500

    Commit

--- app/src/main/java/codigo/parkingfree/RegistrarUsuario.java
+++ app/src/main/java/codigo/parkingfree/RegistrarUsuario.java
@@ -2,6 +2,7 @@ package codigo.parkingfree;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class RegistrarUsuario extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class RegistrarUsuario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar_usuario);
+
+        Toolbar my_ToolBar = findViewById(R.id.myToolBar);
+        setSupportActionBar(my_ToolBar);
+        my_ToolBar.setPadding(0,0,0,0);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 }
