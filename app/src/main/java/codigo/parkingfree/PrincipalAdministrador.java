c7e31ddf54c84c2ac31892bdcde621d5721b3760
DESARROLLO-07 <emgonzalezm@ufpso.edu.co>
2018-11-22 16:20:04 -0500

    Commit

--- app/src/main/java/codigo/parkingfree/PrincipalAdministrador.java
+++ app/src/main/java/codigo/parkingfree/PrincipalAdministrador.java
@@ -1,12 +1,21 @@
 package codigo.parkingfree;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-public class PrincipalAdministrador extends AppCompatActivity {
+public class PrincipalAdministrador extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +25,29 @@ public class PrincipalAdministrador extends AppCompatActivity {
         Toolbar my_ToolBar = findViewById(R.id.myToolBar);
         setSupportActionBar(my_ToolBar);
         my_ToolBar.setPadding(0, 0, 0, 0);
+
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
+        View header = navigationView.getHeaderView(0);
+
+        DrawerLayout myDrawerLayout = findViewById(R.id.drawerLayout);
+        toggle = new ActionBarDrawerToggle(this, myDrawerLayout, R.string.open, R.string.close);
+        myDrawerLayout.addDrawerListener(toggle);
+
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(toggle.onOptionsItemSelected(item)){
+
+            return  true;
+
+        }
 
         switch (item.getItemId()) {
 
@@ -42,5 +69,28 @@ public class PrincipalAdministrador extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_cliente, menu);
 
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+
+        switch (item.getItemId()){
+
+
+            case R.id.opcionEditParqueadero:
+
+                Toast.makeText(this, "opcion 1", Toast.LENGTH_LONG).show();
+
+                break;
+
+        }
+
+        DrawerLayout dl = findViewById(R.id.drawerLayout);
+
+        if(dl.isDrawerOpen(GravityCompat.START)) {
+            dl.closeDrawer(GravityCompat.START);
+        }
+        return false;
     }
 }
