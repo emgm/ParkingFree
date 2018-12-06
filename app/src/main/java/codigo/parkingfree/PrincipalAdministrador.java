cb44163a1db820870b69384b70cded8e1da8ce6c
DESARROLLO-07 <emgonzalezm@ufpso.edu.co>
2018-12-06 16:05:20 -0500

    Commit

--- app/src/main/java/codigo/parkingfree/PrincipalAdministrador.java
+++ app/src/main/java/codigo/parkingfree/PrincipalAdministrador.java
@@ -1,7 +1,6 @@
 package codigo.parkingfree;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -9,11 +8,9 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class PrincipalAdministrador extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -93,7 +90,7 @@ public class PrincipalAdministrador extends AppCompatActivity implements Navigat
 
             case R.id.opcionEditParqueadero:
 
-                startActivity(new Intent(this, RegistrarParqueadero.class));
+                startActivity(new Intent(this, RegistrarOEditarParqueadero.class));
 
                 break;
 
--- app/src/main/java/codigo/parkingfree/RegistrarOEditarParqueadero.java
+++ app/src/main/java/codigo/parkingfree/RegistrarOEditarParqueadero.java
@@ -1,5 +1,7 @@
 package codigo.parkingfree;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -8,13 +10,16 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class RegistrarParqueadero extends AppCompatActivity {
+public class RegistrarOEditarParqueadero extends AppCompatActivity implements View.OnClickListener {
 
     TextView etNombreParqueadero, etDireccion, etHorarioAtencion, etPuestosDisponibles;
-    Button btnRegistrar;
+    Button btnRegistrar, btnEditar;
 
     private FirebaseDatabase mDatabase;
     private DatabaseReference myRef;
@@ -22,7 +27,7 @@ public class RegistrarParqueadero extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registrar_parqueadero);
+        setContentView(R.layout.activity_registrar_o_editar_parqueadero);
 
         Toolbar my_ToolBar = findViewById(R.id.myToolBar);
         setSupportActionBar(my_ToolBar);
@@ -38,9 +43,48 @@ public class RegistrarParqueadero extends AppCompatActivity {
         mDatabase = FirebaseDatabase.getInstance();
 
         btnRegistrar = findViewById(R.id.btnRegistrar);
-        btnRegistrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        btnRegistrar.setOnClickListener(this);
+
+        btnEditar = findViewById(R.id.btnEditar);
+        btnEditar.setOnClickListener(this);
+
+        myRef = mDatabase.getReference("Parqueadero");
+
+        myRef.orderByChild("DocumentoAdministrador")
+                .equalTo(Login.documento)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        if(dataSnapshot.getChildrenCount() > 0) {
+
+                            btnRegistrar.setVisibility(View.GONE);
+                            btnEditar.setVisibility(View.VISIBLE);
+
+                            for (DataSnapshot child : dataSnapshot.getChildren()) {
+
+                                etNombreParqueadero.setText(String.valueOf(child.child("NombreParqueadero").getValue()));
+                                etDireccion.setText(String.valueOf(child.child("Direccion").getValue()));
+                                etHorarioAtencion.setText(String.valueOf(child.child("HorarioAtencion").getValue()));
+                                etPuestosDisponibles.setText(String.valueOf(child.child("CantidadPuestos").getValue()));
+
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+            case R.id.btnRegistrar:
 
                 myRef = mDatabase.getReference("Parqueadero").push();
 
@@ -58,9 +102,17 @@ public class RegistrarParqueadero extends AppCompatActivity {
                 etHorarioAtencion.setText(null);
                 etPuestosDisponibles.setText(null);
 
+                etPuestosDisponibles.clearFocus();
+
                 Toast.makeText(getApplicationContext(), "El parqueadero ha sido registrado.", Toast.LENGTH_SHORT).show();
 
-            }
-        });
+                startActivity(new Intent(getApplicationContext(), PrincipalAdministrador.class));
+
+                break;
+
+            case R.id.btnEditar:
+
+                break;
+        }
     }
 }
